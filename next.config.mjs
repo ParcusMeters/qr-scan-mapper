9fc80091ad5a72986b32a295f0c6c323138c3e4b
marcus <jacko.peters2001@gmail.com>
2025-02-01 17:58:29 +0800

    Updated config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,57 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin';
+
+const nextConfig = {
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    output: 'standalone',
+    async headers() {
+        return [
+            {
+                // Apply these headers to all routes
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'Access-Control-Allow-Origin',
+                        value: '*',
+                    },
+                    {
+                        key: 'Access-Control-Allow-Methods',
+                        value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+                    },
+                    {
+                        key: 'Access-Control-Allow-Headers',
+                        value: '*',
+                    },
+                ],
+            },
+        ];
+    },
+    webpack: (config, { isServer }) => {
+        if (isServer) {
+            config.plugins = [...config.plugins, new PrismaPlugin()];
+        }
+
+        return config;
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'tagify-test.s3.amazonaws.com',
+                port: '',
+                pathname: '**',
+                search: '',
+            },
+            {
+                protocol: 'https',
+                hostname: 'tagify-test.s3.us-east-1.amazonaws.com',
+                port: '',
+                pathname: '**',
+                search: '',
+            },
+        ],
+    },
+};
 
 export default nextConfig;
